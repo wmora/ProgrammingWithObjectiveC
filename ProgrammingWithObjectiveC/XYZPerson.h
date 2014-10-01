b20a8e044425d2a9dde71b94fd42b5dcfbf17a98
William Mora <wilmor24@gmail.com>
2014-09-30 23:19:03 -0300

    Defining classes

---
+++ ProgrammingWithObjectiveC/XYZPerson.h
@@ -0,0 +1,19 @@
+//
+//  XYZPerson.h
+//  ProgrammingWithObjectiveC
+//
+//  Created by William Mora on 30/9/14.
+//  Copyright (c) 2014 William Mora. All rights reserved.
+//
+
+@interface XYZPerson : NSObject
+
+@property NSString *firstName;
+@property NSString *lastName;
+@property NSDate *dateOfBirth;
+
++ (id)person;
+
+- (void)sayHello;
+
+@end
---
+++ ProgrammingWithObjectiveC/XYZPerson.m
@@ -0,0 +1,17 @@
+//
+//  XYZPerson.m
+//  ProgrammingWithObjectiveC
+//
+//  Created by William Mora on 30/9/14.
+//  Copyright (c) 2014 William Mora. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+#import "XYZPerson.h"
+
+@implementation XYZPerson
+
+- (void)sayHello {
+    NSLog(@"Hello, world!");
+}
+
+@end
