00a465d54012b7dfcb4fc981437db8c9a763908b
William Mora <wilmor24@gmail.com>
2014-09-30 22:58:19 -0300

    Initial Commit

---
+++ ProgrammingWithObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ProgrammingWithObjectiveC
+//
+//  Created by William Mora on 30/9/14.
+//  Copyright (c) 2014 William Mora. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
